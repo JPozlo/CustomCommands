aa1995c7ab91d22530e0dbf02154809088122123
collozy <ozlocollins@gmail.com>
2020-07-15 21:46:03 +0300

    Add project files.

---
+++ CustomCommands/Models/Commands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCommands.Models
+{
+    public class Commands
+    {
+        public int MyProperty { get; set; }
+    }
+}
