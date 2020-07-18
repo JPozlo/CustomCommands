689d9f9e1389692292b77e2ea401979c274fb0d4
collozy <ozlocollins@gmail.com>
2020-07-18 22:39:01 +0300

    Refined Repo from Mock to SQL

---
+++ CustomCommands/Data/SQLCommandRepo.cs
@@ -0,0 +1,27 @@
+﻿using CustomCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomCommands.Data
+{
+    public class SQLCommandRepo : ICommandRepo
+    {
+        private readonly CustomCommandsContext _context;
+
+        public SQLCommandRepo(CustomCommandsContext context)
+        {
+            _context = context;
+        }
+        public Command GetCommand(int id)
+        {
+            return _context.Commands.FirstOrDefault(p => p.ID == id);
+        }
+
+        public IEnumerable<Command> GetCommands()
+        {            
+            return _context.Commands.ToList();
+        }
+    }
+}
--- CustomCommands/Startup.cs
+++ CustomCommands/Startup.cs
@@ -31,7 +31,7 @@ namespace CustomCommands
 
             services.AddControllers();
 
-            services.AddScoped<ICommandRepo, MockCommandRepo>();
+            services.AddScoped<ICommandRepo, SQLCommandRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
